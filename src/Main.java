afeeb0de940e6b41b1e7330b9006d852d87d0539
MeJohnGabriel <j.gabrielreisaraujo1@gmail.com>
2026-06-15 19:50:43 -0300

    Variables in Java and +

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+package src;
+
+public class Main {
+
+    //THIS IS MY JAVA PROGRAM(NOT REALLY) 
+    
+    public static void main(String[] args) {
+        System.out.print("Hello, World!\n");
+        // System.out.println(" I am learning JAVA!");
+        // System.out.println(" It is nice!");
+
+        // 1. Declaration
+        int age = 21;
+        int year = 2009;
+        double price = 19999.99;
+        double gpa = 3.1;
+        double temp = 12.5;
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = false;
+        boolean isForSale = true;
+
+        String name = "Gabriel";
+        String food = "Burguer";
+        String email = "j.gabrielreis123@gmail.com";
+        String car = "Honda Civic";
+        String color = "Black"; 
+
+    System.out.println("Your choice is a " + color +" " + year + " "  + car);
+    System.out.println("The price is: "+ currency + price);
+
+    if (isForSale) {
+        System.out.println("There is a " + car + " for sale");
+    }else {
+        System.out.println("The " + car + " is not for sale");
+
+    }
+    }
+
+
+     
+}
