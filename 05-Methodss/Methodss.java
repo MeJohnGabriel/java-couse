0a575d4562484736087cde79b0c293f51efa93d2
MeJohnGabriel <j.gabrielreisaraujo1@gmail.com>
2026-06-17 21:19:33 -0300

    Method, Methods Overload, Variable scope

---
+++ 05-Methodss/Methodss.java
@@ -0,0 +1,85 @@
+public class Methodss {
+    static int x = 3;
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x);
+
+    //STUDY----------METHODS---------------------------------------------- 
+    //     String firstNmame = "John";
+    //     int age = 12;
+
+    //     happyBday(firstNmame, age);  
+        
+    //     double res =  squareMethod(3);
+    //     double resCubed = cube(4);
+    //     String fullName = getFullName("Johnny", "Bravo");
+        
+        
+    //     if (verifyAge(age)) {
+    //         System.out.println("You can drink.");
+    //     } else {
+    //         System.out.println("You are too young.");
+    //     }
+
+
+    //     System.out.println(fullName);
+    //     System.out.println(res);
+    //     System.out.println(resCubed);
+    // }
+
+    // static void happyBday(String name, int age ){
+    //     System.out.println("Happy bday to you!");
+    //     System.out.printf("Happy bday dear %s! \n", name);
+    //     System.out.printf("You are %d years old!", age);
+    //     System.out.println("Happy bday to you!\n");
+
+    // }
+
+    // static double squareMethod(double n){
+    //     return n * n;
+    // }
+
+    // static double cube(double number){
+    //     return number * number *number ;
+    // }
+
+    // static String getFullName(String first , String last){
+    //     return first + " " + last;
+    // }
+
+    // static boolean verifyAge(int age){
+    //   return age >= 18 ? true : false;
+    // 
+    // System.out.println(add(2,3, 5));
+
+
+
+    //STUDY----------METHODS OVERLOAD---------------------------------------------- 
+    // static double add(double a, double b){
+    //     return a + b;
+    // } 
+    // static double add(double a, double b, double c){
+    //     return a + b + c;
+    // } 
+    // String pizza = bakePizza("thick breaded", "cheedar", "pepperoni");
+    // System.out.println(pizza);    
+    
+}
+    static void doSmth(){
+        System.out.println(x);
+    }
+    // static String bakePizza(String bread){
+    //     return bread + "pizza";
+    // } 
+
+    // static String bakePizza(String bread, String chesse){
+    //     return chesse + " "+ bread + " pizza";
+    // }
+
+    // static String bakePizza(String bread, String chesse, String topping){
+    //     return chesse + " "+ bread + " " + topping +  " " + " pizza";
+    // } 
+
+    
+
+}
